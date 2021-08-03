7a515f51f878879ba2336def464a12502c2aaf27
miaoyunlong <miaoyunlong@bytedance.com>
2021-08-03 11:33:41 +0800

    + 二刷 题目1 two sum

--- byself/mac_project/byself/leetcode_practice/2nd/no_1_two_sum.hpp
+++ byself/mac_project/byself/leetcode_practice/2nd/no_1_two_sum.hpp
@@ -4,23 +4,75 @@
 
 using namespace std;
 
+
+
+/*
+ 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ 你可以按任意顺序返回答案。
+
+ 示例 1：
+
+ 输入：nums = [2,7,11,15], target = 9
+ 输出：[0,1]
+ 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ 示例 2：
+
+ 输入：nums = [3,2,4], target = 6
+ 输出：[1,2]
+ 示例 3：
+
+ 输入：nums = [3,3], target = 6
+ 输出：[0,1]
+  
+
+ 提示：
+
+ 2 <= nums.length <= 104
+ -109 <= nums[i] <= 109
+ -109 <= target <= 109
+ 只会存在一个有效答案
+ 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
+ **/
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
         vector<int> res;
-        res = {3,5,6,7};
+        
+        for(int i = 0;i < nums.size();i++)
+        {
+            for(int j = i + 1;j < nums.size();j++)
+            {
+                int sum = nums[i] + nums[j];
+                if(sum == target)
+                {
+                    res = {i,j};
+                    break;
+                }
+            }
+        }
         return res;
     }
     
-    
-    void test()
+    void printVector(const std::vector<int>& arr)
     {
-        vector<int> nums = {3,5};
-        vector<int> res = twoSum(nums,8);
-        for(auto it = res.begin();it != res.end();it++)
+        for(auto it = arr.begin();it != arr.end();it++)
         {
             printf("%d\n",*it);
         }
+        printf("--------\n");
+    }
+    
+    void test()
+    {
+//        vector<int> nums = {2,7,11,15};
+//        vector<int> res = twoSum(nums,9);
+        
+        vector<int> nums = {3,2,4};
+        vector<int> res = twoSum(nums,6);
+        
+        printVector(res);
     }
 };
